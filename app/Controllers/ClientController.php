249128ee1b866b3e1bc1cf364e513f1f8526570d
Etudiant <etudiant@example.com>
2026-07-20 22:38:18 +0300

    Revert "Ajout page d'accueil client (/client/home) et correction sidebar"
    This reverts commit d8760192559f829cd4ae42b08bee07cfe7af3d4e.

--- app/Controllers/ClientController.php
+++ app/Controllers/ClientController.php
@@ -48,28 +48,14 @@ class ClientController extends BaseController
             $session->set('client_id', $client['id']);
             $session->set('client_phone', $client['phone_number']);
 
-            return redirect()->to('/client/home');
+            return redirect()->to('/client/dashboard');
         }
 
         if ($session->get('client_id')) {
-            return redirect()->to('/client/home');
+            return redirect()->to('/client/dashboard');
         }
 
         return view('client/login');
-    }
-
-    public function home()
-    {
-        $session = session();
-        if (! $session->get('client_id')) {
-            return redirect()->to('/client/login');
-        }
-
-        $clientModel = new ClientModel();
-        $client = $clientModel->find($session->get('client_id'));
-
-        $data['client'] = $client;
-        return view('client/home', $data);
     }
 
     public function dashboard()
